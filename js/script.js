c1c6c3cea8ab07231587bd50fb3bff836a436704
school-creative <85816482+school-creative@users.noreply.github.com>
2021-06-15 03:17:41 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+	$(".slider").slick({
+		arrows:false,
+		dots: false,
+  		autoplay: true,
+  		autoplaySpeed: 500,
+  		adaptiveHeight: true,
+  		slidesToShow: 2,
+  		infinite: true,
+  		centerPadding:0,
+  		easing:'easy',
+  		pauseOnFocus: true,
+  		pauseOnHover: true,
+  		pauseDotsHover: true,
+  		
+	});
+});
+
+function ibg(){
+let ibg=document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++) { if(ibg[i].querySelector('img')){ ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')'; } }
+}
+ibg();
+
